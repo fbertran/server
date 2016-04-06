d6d6d6babc79622b4664eff1513bdd267fc61756
Alban Linard <alban@linard.fr>
2016-04-06 09:15:48 +0200

    Set default handlers for app.

--- src/cosy/server/app.lua
+++ src/cosy/server/app.lua
@@ -1,8 +1,38 @@
-local lapis = require("lapis")
-local app = lapis.Application()
+local Lapis  = require "lapis"
+local Config = require "lapis.config".get ()
+local app    = Lapis.Application ()
 
-app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+-- app:before_filter (function ()
+    -- local after_dispatch = require "lapis.nginx.context".after_dispatch
+    -- local to_json        = require "lapis.util".to_json
+--   after_dispatch (function ()
+--     print (to_json (_G.ngx.ctx.performance))
+--   end)
+-- end)
+
+app.handle_404 = function ()
+  return {
+    status = 404,
+    layout = false,
+  }
+end
+
+app.handle_error = function (_, err, trace)
+  print (err, trace)
+  return {
+    status = 500,
+    layout = false,
+  }
+end
+
+app:get ("/", function ()
+  return {
+    json = {
+      captcha = {
+        public_key = Config.captcha.public_key,
+      },
+    }
+  }
 end)
 
 return app
