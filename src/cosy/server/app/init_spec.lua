7fbd144e920a0a1777641eabb871c7ce356cc0cc
Alban Linard <alban@linard.fr>
2016-04-14 22:09:20 +0200

    Try to run mock tests outside macos.

--- src/cosy/server/app/init_spec.lua
+++ src/cosy/server/app/init_spec.lua
@@ -1,19 +1,20 @@
 local Db = require "lapis.db"
 
-local environments = {
-  -- mock = {
-  --   app     = require "cosy.server.app",
-  --   use     = require "lapis.spec".use_test_env,
-  --   request = require "lapis.spec.request".mock_request,
-  -- },
-  server = {
-    app     = nil,
-    use     = require "lapis.spec".use_test_server,
-    request = function (_, ...)
-      return require "lapis.spec.server".request (...)
-    end,
-  },
+local environments = {}
+environments.server = {
+  app     = nil,
+  use     = require "lapis.spec".use_test_server,
+  request = function (_, ...)
+    return require "lapis.spec.server".request (...)
+  end,
 }
+if not os.getenv "Apple_PubSub_Socket_Render" then
+  environments.mock = {
+    app     = require "cosy.server.app",
+    use     = require "lapis.spec".use_test_env,
+    request = require "lapis.spec.request".mock_request,
+  }
+end
 
 for name, environment in pairs (environments) do
   local request = environment.request
