7a8d40469fd024c3bdb01fa4237e829e36ede118
Alban Linard <alban@linard.fr>
2016-04-14 22:13:37 +0200

    Revert tests.

--- src/cosy/server/app/init_spec.lua
+++ src/cosy/server/app/init_spec.lua
@@ -8,13 +8,13 @@ environments.server = {
     return require "lapis.spec.server".request (...)
   end,
 }
-if not os.getenv "Apple_PubSub_Socket_Render" then
-  environments.mock = {
-    app     = require "cosy.server.app",
-    use     = require "lapis.spec".use_test_env,
-    request = require "lapis.spec.request".mock_request,
-  }
-end
+-- if not os.getenv "Apple_PubSub_Socket_Render" then
+--   environments.mock = {
+--     app     = require "cosy.server.app",
+--     use     = require "lapis.spec".use_test_env,
+--     request = require "lapis.spec.request".mock_request,
+--   }
+-- end
 
 for name, environment in pairs (environments) do
   local request = environment.request
