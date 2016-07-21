2d2468ef5fb4eed248a91c9e6bdbc9660311fedc
Alban Linard <alban@linard.fr>
2016-07-21 11:43:07 +0200

    Put wercker-specific configuration within config.lua.

--- config.lua
+++ config.lua
@@ -9,19 +9,19 @@ local common = {
   code_cache  = "on",
   postgres = {
     backend  = "pgmoon",
-    host     = assert (os.getenv "POSTGRES_HOST"),
-    user     = assert (os.getenv "POSTGRES_USER"),
-    password = assert (os.getenv "POSTGRES_PASSWORD"),
-    database = assert (os.getenv "POSTGRES_DATABASE"),
+    host     = assert (os.getenv "POSTGRES_HOST"     or os.getenv "POSTGRES_PORT_5432_TCP_ADDR"   ),
+    user     = assert (os.getenv "POSTGRES_USER"     or os.getenv "POSTGRES_ENV_POSTGRES_USER"    ),
+    password = assert (os.getenv "POSTGRES_PASSWORD" or os.getenv "POSTGRES_ENV_POSTGRES_PASSWORD"),
+    database = assert (os.getenv "POSTGRES_DATABASE" or os.getenv "POSTGRES_ENV_POSTGRES_DATABASE"),
   },
   auth0 = {
     domain        = assert (os.getenv "AUTH0_DOMAIN"),
-    client_id     = assert (os.getenv "AUTH0_ID"),
+    client_id     = assert (os.getenv "AUTH0_ID"    ),
     client_secret = assert (os.getenv "AUTH0_SECRET"),
-    api_token     = assert (os.getenv "AUTH0_TOKEN"),
+    api_token     = assert (os.getenv "AUTH0_TOKEN" ),
   },
   docker = {
-    username = assert (os.getenv "DOCKER_USER"),
+    username = assert (os.getenv "DOCKER_USER"  ),
     api_key  = assert (os.getenv "DOCKER_SECRET"),
   },
   editor = {
