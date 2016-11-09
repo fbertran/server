c365400d8425254e2f714276d18807376e0bf36c
Alban Linard <alban@linard.fr>
2016-11-09 11:29:51 +0100

    Allow configuration of server instance.

--- src/cosy/server/instance.lua
+++ src/cosy/server/instance.lua
@@ -5,45 +5,46 @@ local Hashids = require "hashids"
 local Url     = require "socket.url"
 local Http    = require "cosy.server.http"
 
-local Config = {
-  auth0       = {
-    domain        = assert (os.getenv "AUTH0_DOMAIN"),
-    client_id     = assert (os.getenv "AUTH0_ID"    ),
-    client_secret = assert (os.getenv "AUTH0_SECRET"),
-    api_token     = assert (os.getenv "AUTH0_TOKEN" ),
-  },
-  docker      = {
-    username = assert (os.getenv "DOCKER_USER"  ),
-    api_key  = assert (os.getenv "DOCKER_SECRET"),
-  },
-}
-
-local branch = assert (os.getenv "COSY_BRANCH" or os.getenv "WERCKER_GIT_BRANCH")
-if not branch or branch == "master" then
-  local file = assert (io.popen ("git rev-parse --abbrev-ref HEAD", "r"))
-  branch = assert (file:read "*line")
-  file:close ()
-end
-
 local url = "https://cloud.docker.com"
 local api = url .. "/api/app/v1"
 
 local Instance = {}
 Instance.__index = Instance
 
-function Instance.create ()
+function Instance.create (config)
+  config             = config             or {}
+  config.num_workers = config.num_workers or assert (os.getenv "NPROC")
+  config.auth0       = config.auth0       or {
+    domain        = assert (os.getenv "AUTH0_DOMAIN"),
+    client_id     = assert (os.getenv "AUTH0_ID"    ),
+    client_secret = assert (os.getenv "AUTH0_SECRET"),
+    api_token     = assert (os.getenv "AUTH0_TOKEN" ),
+  }
+  config.docker      = config.docker      or {
+    username      = assert (os.getenv "DOCKER_USER"  ),
+    api_key       = assert (os.getenv "DOCKER_SECRET"),
+  }
+  config.branch      = config.branch      or (function ()
+    local branch = assert (os.getenv "COSY_BRANCH" or os.getenv "WERCKER_GIT_BRANCH")
+    if not branch or branch == "master" then
+      local file = assert (io.popen ("git rev-parse --abbrev-ref HEAD", "r"))
+      branch = assert (file:read "*line")
+      file:close ()
+    end
+    return branch
+  end) ()
   for _ = 1, 5 do
     local instance = setmetatable ({
       docker  = nil,
       server  = nil,
       headers = {
-        ["Authorization"] = "Basic " .. Mime.b64 (Config.docker.username .. ":" .. Config.docker.api_key),
+        ["Authorization"] = "Basic " .. Mime.b64 (config.docker.username .. ":" .. config.docker.api_key),
         ["Accept"       ] = "application/json",
         ["Content-type" ] = "application/json",
       }
     }, Instance)
     -- Create service:
-    local id  = branch .. "-" .. Hashids.new (tostring (os.time ())):encode (666)
+    local id  = config.branch .. "-" .. Hashids.new (tostring (os.time ())):encode (666)
     local stack, stack_status = Http.json {
       url     = api .. "/stack/",
       method  = "POST",
@@ -53,17 +54,17 @@ function Instance.create ()
         services = {
           { name  = "postgres",
             image = "postgres",
-            tags  = { Config.branch },
+            tags  = { config.branch },
           },
           { name  = "redis",
             image = "redis:3.0.7",
-            tags  = { Config.branch },
+            tags  = { config.branch },
           },
           { name  = "api",
             image = Et.render ("cosyverif/server:<%- branch %>", {
-              branch = branch,
+              branch = config.branch,
             }),
-            tags  = { Config.branch },
+            tags  = { config.branch },
             ports = {
               "80:8080",
             },
@@ -72,20 +73,20 @@ function Instance.create ()
               "redis",
             },
             environment = {
-              NPROC             = Config.num_workers,
+              NPROC             = config.num_workers,
               COSY_PREFIX       = "/usr/local",
-              COSY_BRANCH       = branch,
+              COSY_BRANCH       = config.branch,
               REDIS_PORT        = "tcp://redis:6379",
               POSTGRES_PORT     = "tcp://postgres:5432",
               POSTGRES_USER     = "postgres",
               POSTGRES_PASSWORD = "",
               POSTGRES_DATABASE = "postgres",
-              AUTH0_DOMAIN      = Config.auth0.domain,
-              AUTH0_ID          = Config.auth0.client_id,
-              AUTH0_SECRET      = Config.auth0.client_secret,
-              AUTH0_TOKEN       = Config.auth0.api_token,
-              DOCKER_USER       = Config.docker.username,
-              DOCKER_SECRET     = Config.docker.api_key,
+              AUTH0_DOMAIN      = config.auth0.domain,
+              AUTH0_ID          = config.auth0.client_id,
+              AUTH0_SECRET      = config.auth0.client_secret,
+              AUTH0_TOKEN       = config.auth0.api_token,
+              DOCKER_USER       = config.docker.username,
+              DOCKER_SECRET     = config.docker.api_key,
             },
           },
         },
@@ -100,7 +101,7 @@ function Instance.create ()
       headers    = instance.headers,
       timeout    = 5, -- seconds
     }
-    assert (started_status == 202)
+    assert (started_status == 202, started_status)
     assert (instance:find_endpoint ())
     for _ = 1, 30 do
       local _, status = Http.json {
